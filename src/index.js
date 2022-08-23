e52a811b5d8c2240a15fbc206c3c14b33de3422b
Melanie Burger <melanie.brgr@gmail.com>
2022-08-23 08:43:17 +0200

    build(config): :construction_worker: sets up parcel library build
    as changes are made to react-cwc parcel detects them, builds the output distribution file, and the enxtJS app updates to include the new bundle.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
-import 'chemdoodleweb';
+import { useEffect, useId, useRef } from "react"
 
+const useChemDoodle = () => {
+  const ref = useRef(null)
 
+  useEffect(() => {
+    if (ref?.current) {
+      const ctx = ref.current.getContext('2d')
+      ctx.fillStyle = 'blue'
+      ctx.fillRect(10, 10, 150, 100)
+    }
+  }, [ref])
+
+  return {
+    controls: [{
+      props: {
+        id: 'open',
+        key: useId(),
+        children: 'Open',
+      }
+    }],
+    canvas: {
+      props: {
+        id: useId(),
+        ref,
+      }
+    },
+  }
+}
+
+export default useChemDoodle;

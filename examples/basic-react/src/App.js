070ddfa983943770c8cb862107f7860faf9c29a4
Melanie Burger <melanie.brgr@gmail.com>
2022-09-11 18:31:24 +0200

    feat(example): :sparkles: inits use of cwc in react app

--- examples/basic-react/src/App.js
+++ examples/basic-react/src/App.js
@@ -1,4 +1,11 @@
+import { useEffect } from "react";
+
 function App() {
+  useEffect(() => {
+    // eslint-disable-next-line no-undef
+    console.log(ChemDoodle.getVersion());
+  }, []);
+
   return (
     <div>
       <header>

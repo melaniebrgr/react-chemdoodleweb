3451779912da6d3ed676586fe926fba1a57cb3bb
Melanie Burger <melanie.brgr@gmail.com>
2022-09-24 10:12:54 +0300

    feat(example): :sparkles: installs CWC but with error

--- examples/react-dynamic-local/src/App.js
+++ examples/react-dynamic-local/src/App.js
@@ -1,4 +1,16 @@
+import { useEffect } from 'react';
+
 function App() {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = `${process.env.PUBLIC_URL}/ChemDoodleWeb-9.4.0/install/ChemDoodleWeb.js`;
+    document.body.appendChild(script);
+  
+    return () => {
+        document.body.removeChild(script);
+    }
+  }, []);
+
   return (
     <div>
       <header>

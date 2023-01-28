40e137b755e4ab5b9dea9c627e94f71df30506ca
Melanie Burger <melanie.brgr@gmail.com>
2023-01-28 12:21:54 +0100

    chore(react-chemdoodleweb): :rotating_light: fixes lint issues across project

--- packages/react-static-global/src/SketcherCanvasExample.jsx
+++ packages/react-static-global/src/SketcherCanvasExample.jsx
@@ -1,12 +1,12 @@
 import { SketcherCanvas } from 'react-chemdoodleweb'
 
-const canvasOptions = { 
+const canvasOptions = {
   includeToolbar: true,
   oneMolecule: true
 }
 
 function SketcherCanvasExample() {
-  return (<SketcherCanvas id="sketcher-canvas" canvasOptions={canvasOptions} />)
+  return <SketcherCanvas id="sketcher-canvas" canvasOptions={canvasOptions} />
 }
 
-export default SketcherCanvasExample;
+export default SketcherCanvasExample
